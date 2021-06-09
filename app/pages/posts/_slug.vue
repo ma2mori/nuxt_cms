4b649ee2a40acd829ea822be1f2b0311d63b07b7
tumamori6 <tumamori6@gmail.com>
2021-06-09 21:30:01 +0900

    add:meta info

--- app/pages/posts/_slug.vue
+++ app/pages/posts/_slug.vue
@@ -27,6 +27,17 @@ export default {
   head() {
     return {
       title: this.post.fields.title,
+      meta:[
+        { hid: 'description', name: 'description', content: this.post.fields.description },
+        { hid: 'og:site_name', property: 'og:site_name', content: this.post.fields.title },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'https://amazing-curie-4208ac.netlify.app//posts/' + this.post.fields.slug },
+        { hid: 'og:title', property: 'og:title', content: this.post.fields.title },
+        { hid: 'og:description', property: 'og:description', content: this.post.fields.description },
+        { hid: 'og:image', property: 'og:image', content: "https:"+this.post.fields.image.fields.file.url },
+        { hid: 'twitter:card', name: 'twitter:card', content: "summary" },
+        { hid: 'twitter:site', name: 'twitter:site', content: "@"}
+      ]
     }
   },
   mounted() {
